4ad476cc5df1db06698d428682dc5be41ed31693
atp1 <abelpaul12@gmail.com>
2023-01-23 01:05:29 +0100

    add

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -14,7 +14,7 @@ const List = () => {
   const [openDate, setOpenDate] = useState(false);
   const [options, setOptions] = useState(location.state.options);
 
-  return (
+  return(
     <div>
       <Navbar />
       <Header type="list" />
